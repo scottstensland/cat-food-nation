22d04bc9aca8f6be0160d3a79d1b417cae6ae402
Scott Stensland <scottstensland@gmail.com>
2014-06-28 20:20:37 -0400

    modified:   ../package.json 	modified:   iterate_mutate_judge.js

--- src/iterate_mutate_judge.js
+++ src/iterate_mutate_judge.js
@@ -36,20 +36,22 @@ var evolveit = function() {
 // ------------  synthesize an audio buffer  ------------  //
 
 
-SIZE_BUFFER_SOURCE = 256;
+SIZE_BUFFER_SOURCE = 5;
+// SIZE_BUFFER_SOURCE = 256;
 // SIZE_BUFFER_SOURCE = 16384;
 
 
 
+var samples_per_cycle = 5;
 // var samples_per_cycle = 64;
-var samples_per_cycle = 256;
+// var samples_per_cycle = 256;
 
 var source_obj = {};
 
 var source_obj = shared_utils.pop_audio_buffer(SIZE_BUFFER_SOURCE, samples_per_cycle);
 
-// var max_index = 3;
-var max_index = SIZE_BUFFER_SOURCE;
+var max_index = 3;
+// var max_index = SIZE_BUFFER_SOURCE;
 
 for (var index = 0; index < max_index; index++) {
 
@@ -155,13 +157,90 @@ var seed_genome = {
 					};
 
 	shared_utils.diff_buffers(source_obj, audio_genome_synth_obj, diff_spec);
-	// shared_utils.diff_buffers(source_obj, source_obj, diff_spec);
-	// shared_utils.diff_buffers(source_obj, audio_genome_synth_obj);
+	console.log("PRE evolve diff_spec   ", diff_spec);
+
+	// --- now iterate across cycles of mutation + judge until satisfactory goodness is reached
 
 
-	console.log("diff_spec   ", diff_spec);
+	var max_acceptible_diff_per_point = 0.1;
+	var max_attempts_per_point = 10;
+
+	var curr_gene_to_evolve = 0;
+
+	// var size_buffer_this_gene = genome.get_size_buffer_this_gene(curr_gene_to_evolve);
+	var size_buffer_this_gene = 2;
+
+	for (var curr_buffer_index_to_mutate = 0; 
+			 curr_buffer_index_to_mutate < size_buffer_this_gene;
+			 curr_buffer_index_to_mutate++) {
+
+		console.log(curr_buffer_index_to_mutate, " curr_buffer_index_to_mutate _____________");
+
+		var curr_value_buffer_prior = genome.get_value_node_buffer(curr_gene_to_evolve,
+																		 curr_buffer_index_to_mutate);
+
+		console.log("curr_value_buffer_prior   ", curr_value_buffer_prior);
+
+		var curr_value_buffer_source = source_obj.buffer[curr_buffer_index_to_mutate];
+
+		console.log("curr_value_buffer_source   ", curr_value_buffer_source);
+
+		// ---
+
+		var curr_attempt = 0;
+		var min_diff_witnessed_so_var = 9999.99;
+		var putative_new_value_float = curr_value_buffer_prior;
+		var curr_best_new_value = curr_value_buffer_prior;
+		var curr_diff = Math.abs(putative_new_value_float - curr_value_buffer_source);
+
+		console.log(curr_buffer_index_to_mutate, " PRE evolve curr_diff ", curr_diff);
+
+		while (curr_diff > max_acceptible_diff_per_point &&
+			curr_attempt < max_attempts_per_point) {
+
+			putative_new_value_float = genome.get_random_float(-1.0, +1.0);
+
+			curr_diff = Math.abs(putative_new_value_float - curr_value_buffer_source);
+
+			if (curr_diff < min_diff_witnessed_so_var) {
+
+				min_diff_witnessed_so_var = curr_diff;
+				curr_best_new_value = putative_new_value_float;
+			};
+
+			console.log(curr_attempt, min_diff_witnessed_so_var, 
+						" putative_new_value_float   ", putative_new_value_float);
 
 
+			curr_attempt++;
+		};
+
+		console.log(curr_buffer_index_to_mutate, " attempts ", curr_attempt,
+					" MMMMMMM   min_diff_witnessed_so_var ", 
+					  min_diff_witnessed_so_var);
+
+
+
+		console.log(curr_buffer_index_to_mutate, " curr_value_buffer_source   ", curr_value_buffer_source);
+		console.log(curr_buffer_index_to_mutate, " SSSSSSS curr_best_new_value   ", curr_best_new_value);
+
+		// --- update gene buffer with this new value ---  //
+
+		genome.set_value_node_buffer(curr_gene_to_evolve, curr_buffer_index_to_mutate, curr_best_new_value);
+
+		console.log(curr_buffer_index_to_mutate, " GGGGGGG get_value_node_buffer ",
+					genome.get_value_node_buffer(curr_gene_to_evolve, curr_buffer_index_to_mutate))
+	};
+
+	// ---
+
+	// diff_spec = { 
+	// 					extent : "entire",	// diff which portions of buffers
+	// 					master : "left",	// determines which buffer determines buffer length
+	// 				};
+
+	shared_utils.diff_buffers(source_obj, audio_genome_synth_obj, diff_spec);
+	console.log("POST evolve diff_spec   ", diff_spec);
 
 };
 exports.evolveit = evolveit;
